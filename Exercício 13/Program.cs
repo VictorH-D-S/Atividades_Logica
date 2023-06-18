3697ce265e979c4823e42771624d0664b49685ee
Victor H Santos <135677578+VictorH-D-S@users.noreply.github.com>
2023-06-18 02:51:20 -0300

    Add files via upload

---
+++ Exercício 13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Logica_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Algoritimo para a tabuada do 8
+
+            
+            int tabuda_do_8;
+            for (int i = 1; i < 11; i++)
+            {
+                tabuda_do_8 = 8 * i;
+                Console.WriteLine($"8 x {i} = {tabuda_do_8}");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

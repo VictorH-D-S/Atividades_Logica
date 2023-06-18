26372ac4d0ef004e650472540b0519841e2900ff
Victor H Santos <135677578+VictorH-D-S@users.noreply.github.com>
2023-06-18 02:37:07 -0300

    Program.cs

--- Exercício 12/Program.cs
+++ Exercício 12/Program.cs
@@ -10,6 +10,8 @@ namespace Atividade_Logica_12
     {
         static void Main(string[] args)
         {
+            // Teste de mesa
+            
             double x, y;
 
             Console.Write("Digite o primeiro valor: ");
